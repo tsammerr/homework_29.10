74892dbe1dc54b1e8b898210f2fb74fdd456afe7
main <tarasovych.maria@gmail.com>
2022-10-29 14:31:55 +0300

    init: ex_2 fix

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-text = input('text -> ')
-wordsText = input('words -> ')
-words = wordsText.split()
-print(words)
-for word in words:
-    if text.count(word) > 0:
-        origen = word
-        teep = word.upper()
+def word():
+    try:
+        text = input('text -> ')
+        wordsText = input('words -> ')
+        words = wordsText.split()
+        print(words)
+        for word in words:
+          if text.count(word) > 0:
+            origen = word
+            teep = word.upper()
 
-        text = text = text.replace(origen, teep)
-print(text)
+            text = text = text.replace(origen, teep)
+        print(text)
+    except Exception as ex:
+        print(f'Error: {ex}')
+
+word()
