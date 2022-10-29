b7832ec9b8582441f7404852e6671848da2f9b64
main <tarasovych.maria@gmail.com>
2022-10-29 14:27:36 +0300

    init: ex_2

---
+++ main.py
@@ -0,0 +1,11 @@
+text = input('text -> ')
+wordsText = input('words -> ')
+words = wordsText.split()
+print(words)
+for word in words:
+    if text.count(word) > 0:
+        origen = word
+        teep = word.upper()
+
+        text = text = text.replace(origen, teep)
+print(text)
